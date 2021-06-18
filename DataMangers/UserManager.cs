3f103ecc242a610f2edc76469774046b4486b3cd
Thant Nay Lin <thantnaylin1301@gmail.com>
2021-06-18 16:26:05 +0800

    Added DataRepository and UserManager

---
+++ DataMangers/UserManager.cs
@@ -0,0 +1,36 @@
+﻿using CarParkAvailability.Context;
+using CarParkAvailability.Models;
+using CarParkAvailability.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarParkAvailability.DataMangers
+{
+    public class UserManager : IDataRepository<User>
+    {
+        readonly UserContext _userContext;
+
+        public UserManager(UserContext context)
+        {
+            _userContext = context;
+        }
+
+        public void Add(User entity)
+        {
+            _userContext.Users.Add(entity);
+            _userContext.SaveChanges();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _userContext.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _userContext.Users.FirstOrDefault(user => user.UserId == id);
+        }
+    }
+}
---
+++ Repository/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarParkAvailability.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Add(TEntity entity);
+    }
+}
