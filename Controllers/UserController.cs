bd35d40a8c1b0b521b149d4392ba6d607f8759b8
Thant Nay Lin <thantnaylin1301@gmail.com>
2021-06-18 14:29:52 +0800

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using CarParkAvailability.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarParkAvailability.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new[]
+            {
+                new User { FirstName = "Thant", LastName = "Nay Lin", Email = "thantnaylin@email.com", ContactNumber = "0216545" },
+                new User { FirstName = "Thant", LastName = "Nay Lin", Email = "thantnaylin@email.com", ContactNumber = "0216545" },
+                new User { FirstName = "Thant", LastName = "Nay Lin", Email = "thantnaylin@email.com", ContactNumber = "0216545" },
+                new User { FirstName = "Thant", LastName = "Nay Lin", Email = "thantnaylin@email.com", ContactNumber = "0216545" }
+            };
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarParkAvailability.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password length must be minimum 6 characters")]
+        public string Password { get; set; }
+
+        public string ContactNumber { get; set; }
+    }
+}
